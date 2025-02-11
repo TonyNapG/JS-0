de8b1319d0d0dcc8c444bdd92b1e7f530cb61a54
Tu Nombre <an631_123@hotmail.com>
2025-02-11 17:41:23 -0600

    practica resuelta ifs y opera dores

--- tipos_de_datos.js
+++ tipos_de_datos.js
@@ -1,1 +1,20 @@
+// experimentando con lops tipos de datos 
 console.log(typeof 4)
+console.log(typeof 'Hola Mundo!')
+var ejemplo_sum = 10*5+2
+console.log(`el resultado de la suma es ${ejemplo_sum}`)
+console.log(typeof 9.21)
+
+//valores de la practica
+console.log(42)
+console.log('veinticinco')
+console.log(-666)
+console.log(true)
+console.log(0)
+//console.log(")
+
+console.log(null)
+console.log(undefined)
+console.log(FALSE)
+
+// fin practica
